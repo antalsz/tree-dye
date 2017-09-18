7b1daa79b38979611ae8e20522ca4a250f628139
Antal Spector-Zabusky <antal.b.sz@gmail.com>
2017-09-18 07:19:22 -0400

    Expand out `randomNeighbor` for `SquareGridGraph`
    For 1000×1000 images, this seems to save up to ~2 seconds for sum-spreading, and
    ~0 seconds for max-spreading.  The difference is decidedly odd, and I can't
    explain it

--- src/lib/TreeDye/Graph/Grid.hs
+++ src/lib/TreeDye/Graph/Grid.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE TypeFamilies, TypeApplications, RecordWildCards, LambdaCase #-}
+{-# LANGUAGE TypeFamilies, TypeApplications, RecordWildCards, LambdaCase,
+             MultiWayIf #-}
 
 module TreeDye.Graph.Grid (
   SquareGridGraph(..),
@@ -23,8 +24,8 @@ instance Integral i => GraphI (SquareGridGraph i) where
   
   neighbors SquareGridGraph{..} (x,y) =  [(x+1, y)   | x < sqgWidth  - 1]
                                       ++ [(x,   y+1) | y < sqgHeight - 1]
-                                      ++ [(x-1, y)   | x > 0 ]
-                                      ++ [(x,   y-1) | y > 0 ]
+                                      ++ [(x-1, y)   | x > 0]
+                                      ++ [(x,   y-1) | y > 0]
 
 instance Integral i => GraphOrdI (SquareGridGraph i) where
   minVertex _                   = (0,0)
@@ -32,9 +33,49 @@ instance Integral i => GraphOrdI (SquareGridGraph i) where
 
 instance (Integral i, Random i) => GraphRandomI (SquareGridGraph i) where
   randomVertex SquareGridGraph{..} =
-    (,) <$> getRandomR (0, sqgWidth  - 1) <*> getRandomR (0, sqgHeight - 1)
-
-  -- TODO: optimize 'randomNeighbor'?
+    (,) <$> getRandomR (0, sqgWidth - 1) <*> getRandomR (0, sqgHeight - 1)
+  
+  randomNeighbor SquareGridGraph{..} (x,y) =
+    let xMin = x == 0
+        xMax = x == sqgWidth - 1
+        yMin = y == 0
+        yMax = y == sqgHeight - 1
+        
+        {-# INLINE rand #-}
+        rand k f = getRandomR @_ @Word (0,k) <&> f
+    in if | xMin      -> if | yMin      -> rand 1 $ \case
+                                             0 -> (1,   0)
+                                             _ -> (0,   1)
+                            | yMax      -> rand 1 $ \case
+                                             0 -> (1,   y)
+                                             _ -> (0,   y-1)
+                            | otherwise -> rand 2 $ \case
+                                             0 -> (1,   y)
+                                             1 -> (0,   y+1)
+                                             _ -> (0,   y-1)
+          | xMax      -> if | yMin      -> rand 1 $ \case
+                                             0 -> (x,   1)
+                                             _ -> (x-1, 0)
+                            | yMax      -> rand 1 $ \case
+                                             0 -> (x-1, y)
+                                             _ -> (x,   y-1)
+                            | otherwise -> rand 2 $ \case
+                                             0 -> (x,   y+1)
+                                             1 -> (x-1, y)
+                                             _ -> (x,   y-1)
+          | otherwise -> if | yMin      -> rand 2 $ \case
+                                             0 -> (x+1, 0)
+                                             1 -> (x,   1)
+                                             _ -> (x-1, 0)
+                            | yMax      -> rand 2 $ \case
+                                             0 -> (x+1, y)
+                                             1 -> (x-1, y) 
+                                             _ -> (x,   y-1)
+                            | otherwise -> rand 3 $ \case
+                                             0 -> (x+1, y)
+                                             1 -> (x,   y+1)
+                                             2 -> (x-1, y)
+                                             _ -> (x,   y-1)
 
 --------------------------------------------------------------------------------
 
@@ -42,10 +83,12 @@ instance (Integral i, Random i) => GraphRandomI (SquareGridGraph i) where
 wrap_next :: (Ord a, Num a) => a -> a -> a
 wrap_next bound i | i < bound - 1 = i+1
                   | otherwise     = 0
+{-# INLINE wrap_next #-}
 
 wrap_prev :: (Eq a, Num a) => a -> a -> a
 wrap_prev bound 0 = bound-1
 wrap_prev _     i = i-1
+{-# INLINE wrap_prev #-}
 
 -- TODO: Positive
 data WrappingSquareGridGraph n =
